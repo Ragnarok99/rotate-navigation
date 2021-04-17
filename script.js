3a754bb9bc059f367aff6b96452bb773e0e33e9a
emmanuel villegas <emmavillegas@MacBook-Pro-de-emmanuel.local>
2021-04-17 13:52:34 -0500

    create rotate navigation

---
+++ script.js
@@ -0,0 +1,11 @@
+const openMenu = document.getElementById("open");
+const closeMenu = document.getElementById("close");
+const container = document.querySelector(".container");
+console.log(container);
+
+openMenu.addEventListener("click", toggleOpen);
+closeMenu.addEventListener("click", toggleOpen);
+
+function toggleOpen() {
+  container.classList.toggle("show-nav");
+}
